a3a0d3d6f7f2de71ec8e3df064fe3cc7728049ce
AlekseyStarikov <148627614+AlekseyStarikov@users.noreply.github.com>
2023-12-09 23:55:35 +0300

    Update Division.java

--- src/main/java/expressionsolve/lab/Division.java
+++ src/main/java/expressionsolve/lab/Division.java
@@ -1,2 +1,39 @@
-package expressionsolve.lab;public class Division {
+package expressionsolve.lab;
+/**
+ * Этот класс предназначен для сущьности подразделение
+ * @author Алексей Стариков
+ */
+public class Division {
+    String id;
+    String name;
+    /**
+     * Конструктор с параметрами
+     * @param id персональный идентификатор подразделения,
+     * @param name  название подразделения
+     */
+    public Division(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Division\n{" + "\nid=" + id + "\nname=" + name + "\n}";
+    }
 }
